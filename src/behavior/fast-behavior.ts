86284f186c71209c697f5f8dcbc6ce3d7cd9b592
hsnzync <hsnzync_@hotmail.com>
2023-01-21 23:15:37 +0100

    feat: adjusted behavior and added volume to Audio class

--- src/behavior/fast-behavior.ts
+++ src/behavior/fast-behavior.ts
@@ -1,17 +1,17 @@
-import { MoveBehavior } from './move-behavior';
-import { GameObject } from '../game-object';
+import { MoveBehavior } from './move-behavior'
+import { GameObject } from '../game-object'
 
 export class fastBehavior implements MoveBehavior {
-  private enemy: GameObject;
-  private speedX: number;
+  private enemy: GameObject
+  private speedX: number
 
   constructor(enemy: GameObject) {
-    this.enemy = enemy;
-    let r = Math.floor(Math.random() * 3) + 2;
-    this.speedX = r;
+    this.enemy = enemy
+    let r = Math.floor(Math.random() * 3) + 2
+    this.speedX = r
   }
 
   public performUpdate() {
-    this.enemy.posX = this.enemy.posX -= this.speedX;
+    this.enemy.posX = this.enemy.posX -= this.speedX
   }
 }
--- src/utils/audio.ts
+++ src/utils/audio.ts
@@ -3,17 +3,20 @@ import { Howl } from 'howler'
 export class Audio {
   private path: string
   private hasLoop: boolean
+  private volume: number
   private audio: Howl
 
-  constructor(path: string, hasLoop: boolean = false) {
+  constructor(path: string, hasLoop: boolean = false, volume: number = 0.5) {
     this.path = path
     this.hasLoop = hasLoop
+    this.volume = volume
   }
 
   public play(): void {
     this.audio = new Howl({
       src: [this.path],
       loop: this.hasLoop,
+      volume: this.volume,
     })
 
     this.audio.play()
